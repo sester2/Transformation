a3aefd01fa9ae5f2fe7f4213ec5ffe6ff556f81c
Sam Ester <sesester@aemcorp.com>
2026-02-18 20:06:43 -0500

    I am working on developing the data to be displayed.

--- Pipeline/Ingestion/loading_data.py
+++ Pipeline/Ingestion/loading_data.py
@@ -1,31 +1,34 @@
-import requests
 import json 
+import urllib.request
+import tarfile
 import pandas as pd
+from pathlib import Path
+
 #Data source: 
 # --------------    --------------------   -------------------
 
 # Load data from API into DataFrame for further processing
 def load_data():
-    try:   
-        response = requests.get("https://api.example.com/data")
-        response.raise_for_status()  # Check if the request was successful
-        loaded_data = response.json()
-        if loaded_data is None:
-            raise ValueError("No data found in the response")
-    except requests.exceptions.RequestException as e:
-        print(f"Data unseccessfully loaded: {e}")
-        loaded_data = None
-
-    for item in loaded_data:
-        print(item)
-    #---------------     --------------------   -------------------
-    # Convert loaded data to DataFrame for further processing
     try:
-        df = pd.DataFrame(loaded_data)
+        housing_path = Path("https://github.com/ageron/data/raw/main/housing.tgz")
+        if not housing_path.is_file():
+            urllib.request.urlretrieve(housing_path)
+            with tarfile.open(housing_path) as housing_tarball:
+                housing_tarball.extractall(path='datasets')
+        
+        df = pd.read_csv(Path("datasets/housing/housing.csv"))
         if df.empty:
-            raise ValueError("DataFrame is empty after conversion")
+            raise ValueError("DataFrame is empty after loading")
+        else:   
+            print("Data loaded successfully.")
+
     except Exception as e:
-        print(f"Error converting loaded data to DataFrame: {e}")
+        print(f"Error loading data: {e}")
         df = pd.DataFrame()
+
+    return df
+
+#---------------     --------------------   -------------------
+# Convert loaded data to DataFrame for further processing 
 
 #---------------     --------------------   -------------------
